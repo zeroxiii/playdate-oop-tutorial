23afa1d9cf2b2c692f30939615f136640275380b
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2023-10-03 21:31:11 -0400

    Created projectile and shoot ability

--- source/player.lua
+++ source/player.lua
@@ -1,3 +1,5 @@
+import "projectile"
+
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
@@ -10,8 +12,27 @@ function Player:init(x, y, image)
 	self.projectileSpeed = 1
 end
 
+function Player:shoot(x, y)
+	local projectileInstance = Projectile(x, y - 10, self.projectileSpeed)
+	projectileInstance:add()
+end
+
+function Player:ability()
+	local function timerCallback()
+		self:shoot(self.x, self.y)
+	end
+	pd.timer.performAfterDelay(50, timerCallback)
+	pd.timer.performAfterDelay(150, timerCallback)
+end
+
 function Player:update()
 	Player.super.update(self)
+	if pd.buttonJustPressed(pd.kButtonA) then
+		self:shoot(self.x, self.y)
+	end
+	if pd.buttonJustPressed(pd.kButtonB) then
+		self:ability()
+	end
 	if pd.buttonIsPressed(pd.kButtonLeft) then
 		self:moveBy(-self.moveSpeed, 0)
 	elseif pd.buttonIsPressed(pd.kButtonRight) then
